994d172e7cb2aed2b8c4b40683abd161e2cfa668
Jeff Bienstadt <jeffbi@jetstreamsoftware.com>
2019-06-19 23:03:53 -0700

    Add code to process the image. Not tested.

--- edge-device/AIVideoEdgeDevice/EdgeDevice/modules/ProcessorFPGA/grpcserver.py
+++ edge-device/AIVideoEdgeDevice/EdgeDevice/modules/ProcessorFPGA/grpcserver.py
@@ -38,18 +38,42 @@ class ImageServicer(fpgagrpc_pb2_grpc.FpgaGrpcChannelServicer):
         """
         Implementation of the SubmitImage method described in the .proto file.
         """
-        image = request.image
-        #TODO: finish this function!!!
-        #TODO: do stuff with the image and return the response
-        img = cv2.imdecode(image, cv2.IMREAD_COLOR)
-        # image is alread sized to 300x300 so we don't need to resize
-        img = img[:, :, ::-1]
-        img = img - (123, 117, 104)
-        img = np.asarray(img, dtype=np.float32)
-        img = np.expand_dims(img, axis=0)
-        result = client.score_numpy_arrays({'brainwave_ssd_vgg_1_Version_0.1_input_1:0':img},
-                                           outputs=tensor_outputs)
-        classes, scores, bboxes = ssdvgg_utils.postprocess(result, select_threshold=0.5)
+        rv = fpgagrpc_pb2.ImageReply()
+        try:
+            image = request.image
+            img = cv2.imdecode(image, cv2.IMREAD_COLOR)
+            # image is alread sized to 300x300 so we don't need to resize
+            img = img[:, :, ::-1]
+            img = img - (123, 117, 104)
+            img = np.asarray(img, dtype=np.float32)
+            img = np.expand_dims(img, axis=0)
+            result = client.score_numpy_arrays({'brainwave_ssd_vgg_1_Version_0.1_input_1:0':img},
+                                               outputs=tensor_outputs)
+            classes, scores, bboxes = ssdvgg_utils.postprocess(result, select_threshold=0.5)
+            processed_results = {}
+            processed_results["classes"] = classes.tolist()
+            processed_results["scores"] = scores.tolist()
+            processed_results["bboxes"] = bboxes.tolist()
+            print(processed_results)
+
+            # Put results into ImageReply object
+            rv.classes = classes.tolist()
+            rv.scores = scores.tolist()
+            boxes = []
+            for bb in bboxes.tolist():
+                bbox = fpgagrpc_pb2.BoundingBox()
+                bbox.yMin = bb[0]
+                bbox.xMin = bb[1]
+                bbox.yMax = bb[2]
+                bbox.xMax = bb[3]
+                boxes.append(bbox)
+            rv.boxes = boxes
+            rv.error = None
+        except Exception as ex:
+            print("Unexpected error:", ex)
+            rv.error = "Unexpected error"
+
+        return rv
 
     def start_server(self):
         """
